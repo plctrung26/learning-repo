b7ca8d8e231bf6a51d161b25bd9ecafe10b14d41
Trung Phan Lê Chí <21522725@ms.uit.edu.vn>
2025-03-14 16:22:22 +0700

    Finish the Login Page front end (still not fetch api)

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -1,8 +1,48 @@
+import { Button, Input, Space } from "antd"
+import './LoginPage.scss'
+import { useState } from "react";
+
 const LoginPage = () => {
+    const [passwordVisible, setPasswordVisible] = useState(false);
+
     return (
-        <div>
-            This is login Page
-        </div>
+        <div className="page-space">
+            <div className="page-form">
+                <div className="form-icon">
+                    <img src="https://s3.ap-southeast-1.amazonaws.com/nurturewave-be-dev/uploads%2Fimages%2F0b8821d6-1a35-4986-af30-232f74a04b51_download+(2).jpeg " />
+                </div>
+                <div className="form-title">
+                    CMS Login
+                </div>
+                <div className="form-input-space">
+                    <div className="form-input">
+                        <div className="input-title">
+                            Username or email
+                        </div>
+                        <Input
+                            className="input"
+                            placeholder="Username or email"
+                        ></Input>
+                    </div>
+                    <div className="form-input">
+                        <div className="input-title">
+                            Password
+                        </div>
+                        <Input.Password
+                            className="input"
+                            placeholder="input password"
+                            visibilityToggle={{ visible: passwordVisible, onVisibleChange: setPasswordVisible }}
+                        />
+                    </div>
+                    <div className="submit-button-space">
+                        <Button
+                            className="submit-button">
+                            Login
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </div >
     )
 }
 
